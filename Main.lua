b103f355aca0ffe74e4c069dfcea71adf5615f41
wave <89941722+wave-chan@users.noreply.github.com>
2026-05-22 15:03:02 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 return {
-  Configs = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Configs.lua", true))(),
-  Loop = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Loop.lua", true))(),
-  Character = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Character.lua", true))(),
-  Table = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Table.lua", true))(),
+  Configs = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Configs.lua"))(),
+  Loop = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Loop.lua"))(),
+  Character = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Character.lua"))(),
+  Table = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Table.lua"))(),
 }
