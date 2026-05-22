cb77b0789cba845eab18bdff025a47744b02cd6d
wave <89941722+wave-chan@users.noreply.github.com>
2026-05-22 13:33:15 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,4 @@
+return {
+  Configs = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Configs.lua", true))()
+  Loop = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Utils/refs/heads/main/Loop.lua", true))()
+}
