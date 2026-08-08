3f5ffd50d44a44d41a2cb5120c8f5e8ecbbfb666
wave <89941722+wave-chan@users.noreply.github.com>
2026-08-08 02:39:05 +0100

    Update Table.lua

--- Table.lua
+++ Table.lua
@@ -20,7 +20,7 @@ function module:LoadFolder(Folder, Data, IgnoreTable: {})
 
         if Child:IsA("Folder") then
             Data[Child.Name] = Data[Child.Name] or {}
-            Manager.Utils.Table:LoadFolder(Child, Data[Child.Name], IgnoreTable)
+            self:LoadFolder(Child, Data[Child.Name], IgnoreTable)
         elseif Child:IsA("ValueBase") then
             Data[Child.Name] = Child.Value
         end
